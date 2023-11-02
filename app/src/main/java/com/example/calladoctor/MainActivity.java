3d11230f4f8f02ece2028fe198cb107f88ccfea4
Chan Huanyi <chyafs@gmail.com>
2023-11-02 10:48:44 +0800

    Login Layout Complete

--- app/src/main/java/com/example/calladoctor/MainActivity.java
+++ app/src/main/java/com/example/calladoctor/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calladoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, LoginPage.class);
+        startActivity(intent);
 
     }
 }
