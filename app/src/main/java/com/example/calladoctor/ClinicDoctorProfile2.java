09793c9b768e6cc3fbdc802c683a70ec60b65d7e
WeiZhanggg <jaydengoh20@gmail.com>
2023-11-03 17:32:48 +0800

    Did the layouts (clinic_profile,clinic_doctor_profile and clinic_doctor_profile2)

---
+++ app/src/main/java/com/example/calladoctor/ClinicDoctorProfile2.java
@@ -0,0 +1,12 @@
+package com.example.calladoctor;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClinicDoctorProfile2 extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clinic_doctor_profile2);
+    } // Add this closing curly brace
+}
+
