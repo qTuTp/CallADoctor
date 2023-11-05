d547f5ebd28c2ae4de55829e82fcc80203de748f
Chan Huanyi <chyafs@gmail.com>
2023-11-05 19:12:25 +0800

    Complete the layout of Patient Edit Profile Page

--- app/src/main/java/com/example/calladoctor/PatientProfilePage.java
+++ app/src/main/java/com/example/calladoctor/PatientProfilePage.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.example.calladoctor.Class.Patient;
 import com.google.android.material.button.MaterialButton;
 
 public class PatientProfilePage extends AppCompatActivity {
 
     private EditText nameEditText, icEditText, birthDateEditText, genderEditText, phoneEditText, emailEditText, addressEditText;
     private MaterialButton editProfileButton, changePasswordButton, changeEmailButton;
+    private Patient patient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,31 @@ public class PatientProfilePage extends AppCompatActivity {
         setContentView(R.layout.activity_patient_profile_page);
 
         setReference();
+
+        //Place holder data, suppose the data should be saved when login using shared preference
+        patient = new Patient(
+                "P001",
+                "1234567890",
+                "John",
+                "Doe",
+                "2000-12-31",
+                "Male",
+                "1234567890",
+                "john.doe@example.com",
+                "123 Main Street, City"
+        );
+
+
+        //Updating the profile with the patient information
+        String name = patient.getfName() + " " + patient.getlName();
+        nameEditText.setText(name);
+        icEditText.setText(patient.getIc());
+        birthDateEditText.setText(patient.getBirthDate());
+        genderEditText.setText(patient.getGender());
+        phoneEditText.setText(patient.getPhoneNo());
+        emailEditText.setText(patient.getEmail());
+        addressEditText.setText(patient.getAddress());
+
     }
 
     private void setReference(){
