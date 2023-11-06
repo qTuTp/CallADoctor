092b4f583858a1cd7e594cbf53d883f00c670000
Chan Huanyi <chyafs@gmail.com>
2023-11-07 03:44:56 +0800

    Add date picker for registration page when selecting birth date

--- app/src/main/java/com/example/calladoctor/MainActivity.java
+++ app/src/main/java/com/example/calladoctor/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, ClinicAppointmentPendingDetail.class);
+        Intent intent = new Intent(MainActivity.this, LoginPage.class);
         startActivity(intent);
         finish();
 
