f229e82cce2010041a5627c9a564fd205d51f535
KaiMinBong <p21013393@student.newinti.edu.my>
2023-11-06 20:28:23 +0800

    added a new layout called doctor follow up appointment (still need some adjustments)

---
+++ app/src/main/java/com/example/calladoctor/DoctorFollowUpAppointment.java
@@ -0,0 +1,14 @@
+package com.example.calladoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorFollowUpAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_follow_up_appointment);
+    }
+}
