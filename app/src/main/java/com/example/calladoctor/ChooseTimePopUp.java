d7d1406bb9176400cc174ab01ed3e9672136d6c0
Melvin0526 <limming0526@gmail.com>
2023-11-06 21:51:22 +0800

    - Complete the clinic homepage but not yet add the navigation bar - Complete the layout of all the pop up - Complete the layout of clinic appointment pending detail - Complete the function of pop up

--- app/src/main/java/com/example/calladoctor/ChooseTimePopUp.java
+++ app/src/main/java/com/example/calladoctor/ChooseTimePopUp.java
@@ -16,47 +16,10 @@ import java.util.Locale;
 
 public class ChooseTimePopUp extends AppCompatActivity {
 
-    AppCompatButton timeButton;
-    int hour,minute;
-    AppCompatButton popupButton;
-    Dialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_time_pop_up);
-        timeButton = findViewById(R.id.timeButton);
-
-        popupButton = findViewById(R.id.changeTimeButton);
-        mDialog = new Dialog(this);
-        popupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                mDialog.setContentView(R.layout.change_time_pop_up);
-                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                mDialog.show();
-            }
-        });
-
-    }
-
-    public void popTimePicker(View view) {
-        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
-                hour = selectedHour;
-                minute = selectedMinute;
-                timeButton.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
-
-            }
-        };
-
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this,style,onTimeSetListener,hour,minute,true);
-        timePickerDialog.setTitle("Select Time");
-        timePickerDialog.show();
-
     }
 }
---
+++ app/src/main/java/com/example/calladoctor/RejectAppointmentPopUp.java
@@ -0,0 +1,14 @@
+package com.example.calladoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RejectAppointmentPopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reject_appointment_pop_up);
+    }
+}
