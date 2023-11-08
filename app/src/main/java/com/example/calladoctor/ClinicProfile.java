741651488663b6f8125e210c810496e5927f3f0c
WeiZhanggg <jaydengoh20@gmail.com>
2023-11-08 09:10:26 +0800

    Update on clinic profile page

--- app/src/main/java/com/example/calladoctor/ClinicProfile.java
+++ app/src/main/java/com/example/calladoctor/ClinicProfile.java
@@ -1,8 +1,15 @@
 package com.example.calladoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class ClinicProfile extends AppCompatActivity {
 
@@ -10,5 +17,9 @@ public class ClinicProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.clinic_profile);
+
+        RecyclerView recyclerView = findViewById(R.id.recycle_view);
+
+
     }
 }
