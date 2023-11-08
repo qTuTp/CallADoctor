2da743c62e81f55608a10a2ec9aca50eafe31777
WeiZhanggg <jaydengoh20@gmail.com>
2023-11-08 09:51:55 +0800

    Update on clinic profile page Update on clinic doctor edit page

--- app/src/main/java/com/example/calladoctor/ClinicDoctorEditProfile.java
+++ app/src/main/java/com/example/calladoctor/ClinicDoctorEditProfile.java
@@ -3,10 +3,10 @@ package com.example.calladoctor;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ClinicDoctorProfile2 extends AppCompatActivity {
+public class ClinicDoctorEditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.clinic_doctor_profile2);
+        setContentView(R.layout.clinic_doctor_editprofile);
     }
 }
 
