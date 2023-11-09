d4e6be63b1d4fe5ee034d5a91784eaa338b10860
KaiMinBong <p21013393@student.newinti.edu.my>
2023-11-09 22:10:08 +0800

    completed the layout called doctor accepted appointment

---
+++ app/src/main/java/com/example/calladoctor/DoctorAcceptedAppointment.java
@@ -0,0 +1,13 @@
+package com.example.calladoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoctorAcceptedAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_accepted_appointment);
+    }
+}
