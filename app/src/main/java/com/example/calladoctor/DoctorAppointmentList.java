5687a89c048445256d8dd1f0381209eb4f35f44e
KaiMinBong <p21013393@student.newinti.edu.my>
2023-11-10 01:46:44 +0800

    completed the layout for doctor appointment list

---
+++ app/src/main/java/com/example/calladoctor/DoctorAppointmentList.java
@@ -0,0 +1,13 @@
+package com.example.calladoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoctorAppointmentList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_appointment_list);
+    }
+}
