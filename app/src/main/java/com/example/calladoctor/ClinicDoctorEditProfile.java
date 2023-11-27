fcb0ffc0dbd74439e1b80ef0cdad9535e43d9039
KaiMinBong <p21013393@student.newinti.edu.my>
2023-11-27 23:08:50 +0800

    add the backend of recycler view for clinic home page to get appointment information in firebase and display it. (incomplete)
    connected the edit doctor profile page and enabled it to retrieve doctor information to allow clinic to edit it. (incomplete)

--- app/src/main/java/com/example/calladoctor/ClinicDoctorEditProfile.java
+++ app/src/main/java/com/example/calladoctor/ClinicDoctorEditProfile.java
@@ -1,37 +1,46 @@
 package com.example.calladoctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioGroup;
-import android.widget.Toast;
-import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 
 public class ClinicDoctorEditProfile extends AppCompatActivity {
-    private TextInputLayout name;
-    private TextInputLayout firstName;
-    private TextInputLayout lastName;
-    private TextInputLayout icNo;
-    private TextInputLayout phoneNo;
-    private TextInputLayout address;
-    private TextInputLayout birthDate;
+    private TextInputEditText name;
+    private TextInputEditText txt_fName;
+    private TextInputEditText txt_lName;
+//    private TextInputLayout firstName;
+//    private TextInputLayout lastName;
+//    private TextInputLayout icNo;
+    private TextInputEditText txt_icNo;
+//    private TextInputLayout phoneNo;
+    private TextInputEditText txt_phoneNo;
+//    private TextInputLayout address;
+    private TextInputEditText txt_Address;
+//    private TextInputLayout birthDate;
+    private TextInputEditText txt_birthDate;
     private MaterialDatePicker<Long> birthDatePicker;
     private View birthDateClickable;
     private RadioGroup gender;
@@ -49,36 +58,41 @@ public class ClinicDoctorEditProfile extends AppCompatActivity {
     }
 
     private void updateData(){
-        SharedPreferences prefs = getSharedPreferences("doctors_detail", Context.MODE_PRIVATE);
-        String nameStr = prefs.getString("userName", "");
-        String icNoStr = prefs.getString("icNo", "");
-        String phoneStr = prefs.getString("phone", "");
-        String addressStr = prefs.getString("address","");
-        String birthdateStr = prefs.getString("birthdate","");
-        String firstNameStr = prefs.getString("firstName", "");
-        String lastNameStr = prefs.getString("lastName", "");
+        Intent intent = getIntent();
+        String nameStr = intent.getExtras().getString("userName");
+        String icNoStr = intent.getExtras().getString("icNo");
+        String phoneStr = intent.getExtras().getString("phone");
+        String addressStr = intent.getExtras().getString("address");
+        String birthdateStr = intent.getExtras().getString("birthdate");
+//        name.setText(nameStr);
 
+        String[] names = nameStr.split(" ");
 
+        if (names.length >= 1) {
+            // Assign last index part to firstName
+            txt_lName.setText(names[names.length - 1]);
+        }
+        if (names.length >= 2) {
+            // Join the front parts before the last name
+            txt_fName.setText(String.join(" ", Arrays.copyOfRange(names, 0, names.length - 1)));
+        }
 
-        name.getEditText().setText(nameStr);
-        icNo.getEditText().setText(icNoStr);
-        address.getEditText().setText(addressStr);
-        phoneNo.getEditText().setText(phoneStr );
-        birthDate.getEditText().setText(birthdateStr);
-        firstName.getEditText().setText(firstNameStr);
-        lastName.getEditText().setText(lastNameStr);
+        txt_icNo.setText(icNoStr);
+        txt_Address.setText(addressStr);
+        txt_phoneNo.setText(phoneStr );
+        txt_birthDate.setText(birthdateStr);
 
     }
 
     private void setReference(){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        firstName = findViewById(R.id.firstName);
-        lastName = findViewById(R.id.lastName);
-        icNo = findViewById(R.id.icNo);
-        phoneNo = findViewById(R.id.phoneNo);
-        address = findViewById(R.id.address);
-        birthDate = findViewById(R.id.birthDate);
+        txt_fName = findViewById(R.id.txt_fName);
+        txt_lName = findViewById(R.id.txt_lName);
+        txt_icNo = findViewById(R.id.txt_icNo);
+        txt_phoneNo = findViewById(R.id.txt_phoneNo);
+        txt_Address = findViewById(R.id.txt_Address);
+        txt_birthDate = findViewById(R.id.txt_birthDate);
         gender = findViewById(R.id.gender);
         saveButton = findViewById(R.id.saveButton);
         backButton = findViewById(R.id.backButton);
@@ -95,92 +109,119 @@ public class ClinicDoctorEditProfile extends AppCompatActivity {
             finish();
         });
 
-        birthDateClickable.setOnClickListener(v -> {
-            birthDatePicker.show(getSupportFragmentManager(), birthDatePicker.toString());
-        });
+//        birthDateClickable.setOnClickListener(v -> {
+//            birthDatePicker.show(getSupportFragmentManager(), birthDatePicker.toString());
+//        });
     }
 
     private void validateData(){
         name.setError(null);
-        phoneNo.setError(null);
-        address .setError(null);
-        birthDate.setError(null);
-        icNo.setError(null);
-        firstName.setError(null);
-        lastName.setError(null);
+        txt_phoneNo.setError(null);
+        txt_Address .setError(null);
+        txt_birthDate.setError(null);
+        txt_icNo.setError(null);
+        txt_fName.setError(null);
+        txt_lName.setError(null);
 
-        SharedPreferences prefs = getSharedPreferences("doctors_detail", Context.MODE_PRIVATE);
-        String nameStr = prefs.getString("Name", "");
-        String icNoStr = prefs.getString("icNo", "");
-        String phoneStr = prefs.getString("phone", "");
-        String addressStr = prefs.getString("address","");
-        String birthdateStr = prefs.getString("birthdate","");
-        String firstNameStr = prefs.getString("firstName", "");
-        String lastNameStr = prefs.getString("lastName", "");
+        Intent intent = getIntent();
+        String nameStr = intent.getExtras().getString("userName");
+        String icNoStr = intent.getExtras().getString("icNo");
+        String phoneStr = intent.getExtras().getString("phone");
+        String addressStr = intent.getExtras().getString("address");
+        String birthdateStr = intent.getExtras().getString("birthdate");
+        String docID = intent.getExtras().getString("clinicID");
 
+        String editPhoneStr = String.valueOf(txt_phoneNo.getText());
+        String editAddressStr = String.valueOf(txt_Address.getText());
+        String editbirthdateStr = String.valueOf(txt_birthDate.getText());
+        String editicNoStr = String.valueOf(txt_icNo.getText());
+        String editFirstNameStr = String.valueOf(txt_fName.getText());
+        String editLastNameStr = String.valueOf(txt_lName.getText());
 
+        String[] names = nameStr.split(" ");
 
+        String firstName1 = "";
+        String lastName1 = "";
 
-        String editPhoneStr = phoneNo.getEditText().getText().toString().trim();
-        String editAddressStr = address.getEditText().getText().toString().trim();
-        String editbirthdateStr = birthDate.getEditText().getText().toString().trim();
-        String editicNoStr = icNo.getEditText().getText().toString().trim();
-        String editFirstNameStr = firstName.getEditText().getText().toString().trim();
-        String editLastNameStr = lastName.getEditText().getText().toString().trim();
-
-
-
-        if (birthdateStr.equals("some_value")) {
-            // Do something
-        } else {
-            // Do something else
+        if (names.length >= 1) {
+            // Assign last index part to firstName
+            lastName1 = names[names.length - 1];
         }
-
-        if (addressStr.equals("some_value")) {
-            // Do something
-        } else {
-            // Do something else
+        if (names.length >= 2) {
+            // Join the front parts before the last name
+            firstName1 = String.join(" ", Arrays.copyOfRange(names, 0, names.length - 1));
         }
-
-        if (icNoStr.equals("some_value")) {
-            // Do something
-        } else {
-            // Do something else
-        }
-
 
         if (editFirstNameStr.isEmpty()){
-            name.setError("Name is Required !");
-            return;
+            txt_fName.setText(firstName1);
+            editFirstNameStr = firstName1;
         }
         if (editLastNameStr.isEmpty()){
-            name.setError("Name is Required !");
-            return;
+            txt_lName.setText(lastName1);
+            editLastNameStr = lastName1;
         }
 
         if (editPhoneStr.isEmpty()){
-            phoneNo.setError("Phone is Required !");
-            return;
+            txt_phoneNo.setText(phoneStr);
+            editPhoneStr = phoneStr;
         }
         if (editAddressStr.isEmpty()){
-            name.setError("Name is Required !");
-            return;
+            txt_Address.setText(addressStr);
+            editAddressStr = addressStr;
         }
         if (editbirthdateStr.isEmpty()){
-            name.setError("Name is Required !");
-            return;
+            txt_birthDate.setText(birthdateStr);
+            editbirthdateStr = birthdateStr;
         }
         if (editicNoStr.isEmpty()){
-            name.setError("Name is Required !");
-            return;
+            txt_icNo.setText(icNoStr);
+            editicNoStr = icNoStr;
         }
 
 
         String Name = editFirstNameStr + " " + editLastNameStr;
-        saveDataToFirestore(editFirstNameStr, editPhoneStr,editAddressStr,editbirthdateStr,editicNoStr, editLastNameStr);
-
+//        saveDataToFirestore(editFirstNameStr, editPhoneStr,editAddressStr,editbirthdateStr,editicNoStr, editLastNameStr);
+        getDocumentId(docID, editPhoneStr, editAddressStr, editbirthdateStr, editicNoStr, editFirstNameStr, editLastNameStr);
 
     }
+
+    private void getDocumentId(String docID, String editPhoneStr, String editAddressStr, String editbirthdateStr, String editicNoStr, String editFirstNameStr, String editLastNameStr) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference users = db.collection("users");
+        users.whereEqualTo("clinicID", docID).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // Get the document ID
+                                String newDocId = document.getId();
+                                updateStatus(newDocId, editPhoneStr, editAddressStr, editbirthdateStr, editicNoStr, editFirstNameStr, editLastNameStr);
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void updateStatus(String newDocId, String editPhoneStr, String editAddressStr, String editbirthdateStr, String editicNoStr, String editFirstNameStr, String editLastNameStr) {
+
+        FirebaseFirestore.getInstance().collection("appointment").document(newDocId)
+                .update(
+                        "phone", editPhoneStr,
+                        "address", editAddressStr,
+                        "birthDate", editbirthdateStr,
+                        "ic", editicNoStr,
+                        "firstName", editFirstNameStr,
+                        "lastName", editLastNameStr
+                )
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        finish();
+                    }
+                });
+    }
+
     private void saveDataToFirestore(String newicNo, String newFirstName, String newPhone, String newaddress, String newbirthdate, String newLastName) {
         // Initialize Firestore
         FirebaseFirestore db = FirebaseFirestore.getInstance();
--- app/src/main/java/com/example/calladoctor/ClinicHomePage.java
+++ app/src/main/java/com/example/calladoctor/ClinicHomePage.java
@@ -10,8 +10,14 @@ import android.os.Bundle;
 import com.example.calladoctor.Class.Appointment;
 import com.example.calladoctor.Class.HomePageAdapter;
 import com.example.calladoctor.Class.Patient;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.example.calladoctor.Interface.OnItemClickedListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 import java.time.LocalDate;
@@ -38,207 +44,70 @@ public class ClinicHomePage extends AppCompatActivity implements OnItemClickedLi
 
         setReference();
 
-        //Place holder Data
-        Appointment appointment1 = new Appointment(
-                "A001",
-                new Patient("P001", "1234567890", "John", "Doe", "2000-03-12", "Male", "1234567890", "john.doe@example.com", "123 Main St", null),
-                "Dr. Smith",
-                "City Clinic",
-                LocalTime.of(10, 0),
-                LocalDate.of(2023, 11, 1),
-                LocalTime.of(11, 0),
-                LocalDate.of(2023, 11, 2),
-                LocalTime.of(12, 0),
-                LocalDate.of(2023, 11, 3),
-                LocalTime.of(13, 0),
-                LocalDate.of(2023, 11, 4),
-                "Completed",
-                "Regular checkup"
-        );
+        String clinicID = "C4QrwMRX1FGEFi9C9J8d";
+        List<String> docIDArray = new ArrayList<String>();
 
-        Appointment appointment2 = new Appointment(
-                "A002",
-                new Patient("P002", "9876543210", "Alice", "Johnson", "1995-08-20", "Female", "9876543210", "alice.johnson@example.com", "456 Elm St", null),
-                "Dr. Lee",
-                "Downtown Medical Center",
-                LocalTime.of(14, 30),
-                LocalDate.of(2023, 11, 5),
-                LocalTime.of(15, 0),
-                LocalDate.of(2023, 11, 7),
-                null,
-                null,
-                null,
-                null,
-                "Pending",
-                "Follow-up appointment"
-        );
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference users = db.collection("users");
+        CollectionReference appointment = db.collection("appointment");
+        // Reference to your collection
+        users.whereArrayContains("clinicID", clinicID)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Access the document ID
+                            String documentId = document.getId();
+                            appointment.get()
+                                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                            for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                                Appointment data = documentSnapshot.toObject(Appointment.class);
+                                                fetchedAppointmentList.add(data);
+                                            }
+                                            for (Appointment appointment : fetchedAppointmentList) {
+                                                if (Objects.equals(appointment.getPat(), documentId) && isMoreThanOneDayApart(LocalDate.parse(appointment.getDateRq()))) {
+                                                    overtimeAppointmentList.add(appointment);
+                                                } else
+                                                    regularAppointmentList.add(appointment);
+                                            }
+                                        }
+                                    });
 
-        Appointment appointment3 = new Appointment(
-                "A003",
-                new Patient("P003", "5551234567", "Michael", "Brown", "1982-06-15", "Male", "5551234567", "michael.brown@example.com", "789 Oak St", null),
-                "Dr. Patel",
-                "Family Health Clinic",
-                LocalTime.of(9, 30),
-                LocalDate.of(2023, 11, 10),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Upcoming",
-                "Annual physical exam"
-        );
+                        }
+                        overtimeAppointmentListAdapter = new HomePageAdapter(ClinicHomePage.this, overtimeAppointmentList, ClinicHomePage.this);
+                        regularAppointmentListAdapter = new HomePageAdapter(ClinicHomePage.this, regularAppointmentList, ClinicHomePage.this);
 
-        Appointment appointment4 = new Appointment(
-                "A004",
-                new Patient("P004", "3337779990", "Emily", "Wilson", "1998-11-28", "Female", "3337779990", "emily.wilson@example.com", "321 Pine St", null),
-                "Dr. Garcia",
-                "Wellness Center",
-                LocalTime.of(11, 15),
-                LocalDate.of(2023, 11, 14),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Pending",
-                "Blood test appointment"
-        );
+                        overtimeRequestRV.setAdapter(overtimeAppointmentListAdapter);
+                        appointmentListRV.setAdapter(regularAppointmentListAdapter);
+                        overtimeRequestRV.setLayoutManager(new LinearLayoutManager(ClinicHomePage.this));
+                        appointmentListRV.setLayoutManager(new LinearLayoutManager(ClinicHomePage.this));
+                    }
+                });
 
-        Appointment appointment5 = new Appointment(
-                "A005",
-                new Patient("P005", "4448882221", "David", "Smith", "1975-04-03", "Male", "4448882221", "david.smith@example.com", "567 Cedar St", null),
-                "Dr. Turner",
-                "Sunset Health Center",
-                LocalTime.of(16, 0),
-                LocalDate.of(2023, 11, 20),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Pending",
-                "X-ray appointment"
-        );
 
-        Appointment appointment6 = new Appointment(
-                "A006",
-                new Patient("P006", "1112223334", "Emma", "Anderson", "1990-09-08", "Female", "1112223334", "emma.anderson@example.com", "987 Birch St", null),
-                "Dr. Johnson",
-                "Urgent Care Clinic",
-                LocalTime.of(17, 45),
-                LocalDate.of(2023, 11, 25),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Denied",
-                "All Time Occupied"
-        );
-
-        Appointment appointment7 = new Appointment(
-                "A007",
-                new Patient("P007", "6669995558", "Matthew", "Davis", "1989-02-10", "Male", "6669995558", "matthew.davis@example.com", "741 Maple St", null),
-                "Dr. White",
-                "Children's Hospital",
-                LocalTime.of(13, 30),
-                LocalDate.of(2023, 11, 29),
-                LocalTime.of(13, 30),
-                LocalDate.of(2023, 11, 29),
-                LocalTime.of(13, 30),
-                LocalDate.of(2023, 11, 29),
-                null,
-                null,
-                "Upcoming",
-                "Pediatric checkup"
-        );
-
-        Appointment appointment8 = new Appointment(
-                "A008",
-                new Patient("P008", "7773332226", "Olivia", "Martinez", "1996-12-17", "Female", "7773332226", "olivia.martinez@example.com", "852 Willow St", null),
-                "Dr. Harris",
-                "Dental Care Center",
-                LocalTime.of(9, 0),
-                LocalDate.of(2023, 12, 2),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Pending",
-                "Dental cleaning"
-        );
-
-        Appointment appointment9 = new Appointment(
-                "A009",
-                new Patient("P009", "2227775550", "James", "Jones", "1980-07-04", "Male", "2227775550", "james.jones@example.com", "123 Spruce St", null),
-                "Dr. Lewis",
-                "Orthopedic Clinic",
-                LocalTime.of(15, 45),
-                LocalDate.of(2023, 12, 6),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Pending",
-                "Orthopedic consultation"
-        );
-
-        Appointment appointment10 = new Appointment(
-                "A010",
-                new Patient("P010", "4441117770", "Sophia", "Jackson", "1993-01-22", "Female", "4441117770", "sophia.jackson@example.com", "456 Pine St", null),
-                "Dr. Moore",
-                "Cardiology Center",
-                LocalTime.of(10, 30),
-                LocalDate.of(2023, 12, 10),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Pending",
-                "Cardiac checkup"
-        );
-
-        fetchedAppointmentList.add(appointment1);
-        fetchedAppointmentList.add(appointment2);
-        fetchedAppointmentList.add(appointment3);
-        fetchedAppointmentList.add(appointment4);
-        fetchedAppointmentList.add(appointment5);
-        fetchedAppointmentList.add(appointment6);
-        fetchedAppointmentList.add(appointment7);
-        fetchedAppointmentList.add(appointment8);
-        fetchedAppointmentList.add(appointment9);
-        fetchedAppointmentList.add(appointment10);
-
-        for(Appointment appointment : fetchedAppointmentList){
-            if (Objects.equals(appointment.getStatus(), "Pending"))
-                overtimeAppointmentList.add(appointment);
-            else
-                regularAppointmentList.add(appointment);
-        }
-
-        regularAppointmentListAdapter = new HomePageAdapter(this, regularAppointmentList, this);
-        overtimeAppointmentListAdapter = new HomePageAdapter(this, overtimeAppointmentList, this);
-
-        overtimeRequestRV.setAdapter(overtimeAppointmentListAdapter);
-        appointmentListRV.setAdapter(regularAppointmentListAdapter);
-        overtimeRequestRV.setLayoutManager(new LinearLayoutManager(this));
-        appointmentListRV.setLayoutManager(new LinearLayoutManager(this));
+//        regularAppointmentListAdapter = new HomePageAdapter(this, regularAppointmentList, this);
+//        overtimeAppointmentListAdapter = new HomePageAdapter(this, overtimeAppointmentList, this);
+//
+//        overtimeRequestRV.setAdapter(overtimeAppointmentListAdapter);
+//        appointmentListRV.setAdapter(regularAppointmentListAdapter);
+//        overtimeRequestRV.setLayoutManager(new LinearLayoutManager(this));
+//        appointmentListRV.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
+
+    private boolean isMoreThanOneDayApart(LocalDate targetDate) {
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+
+        // // Check if the target date is more than one day in the past compared to the current date
+        return targetDate.plusDays(1).isBefore(currentDate);
+    }
+
     private void setReference(){
-        nav = findViewById(R.id.clinic_bottom_navigation);
+        nav = findViewById(R.id.navigationBar);
 
         setupNavigationBar();
 
