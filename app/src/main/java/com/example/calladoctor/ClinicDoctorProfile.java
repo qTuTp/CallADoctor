66865fe53ca62e5eb61a541517e71dd9e0343168
Chan Huanyi <chyafs@gmail.com>
2023-11-28 14:53:18 +0800

    Fix Clinic add doctor not validating field

--- app/src/main/java/com/example/calladoctor/ClinicDoctorProfile.java
+++ app/src/main/java/com/example/calladoctor/ClinicDoctorProfile.java
@@ -1,11 +1,14 @@
 package com.example.calladoctor;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -14,6 +17,7 @@ import com.example.calladoctor.Class.Doctor;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.FirebaseFirestore;
 //import com.google.firebase.auth.FirebaseAuth;
 
 public class ClinicDoctorProfile extends AppCompatActivity {
@@ -26,9 +30,11 @@ public class ClinicDoctorProfile extends AppCompatActivity {
     private TextView address;
     private TextView gender;
     private AppCompatButton editProfileButton;
+    private AppCompatButton deleteProfileButton;
 //    private FirebaseAuth auth;
     private Doctor doctor;
     private BottomNavigationView nav;
+    FirebaseFirestore db;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,12 +76,13 @@ public class ClinicDoctorProfile extends AppCompatActivity {
         address = findViewById(R.id.doctor_profile_address);
         birthDate = findViewById(R.id.doctor_profile_birthdate);
         editProfileButton = findViewById(R.id.editProfileButton);
+        deleteProfileButton = findViewById(R.id.DeleteProfileButton);
         gender = findViewById(R.id.doctor_profile_gender);
+
+        db = FirebaseFirestore.getInstance();
 
         nav = findViewById(R.id.bottom_navigation);
         setupNavigationBar();
-
-
 
         editProfileButton.setOnClickListener(view -> {
             Intent intent = new Intent(ClinicDoctorProfile.this, ClinicDoctorEditProfile.class);
@@ -85,6 +92,10 @@ public class ClinicDoctorProfile extends AppCompatActivity {
             intent.putExtra("doctor", doctor);
             startActivity(intent);
             finish();
+        });
+
+        deleteProfileButton.setOnClickListener(v -> {
+            showRemoveDoctorConfirmationDialog();
         });
 
 
@@ -124,5 +135,53 @@ public class ClinicDoctorProfile extends AppCompatActivity {
                 return false;
         });
 
+    }
+    private void showRemoveDoctorConfirmationDialog(){
+        Dialog removeDoctorConfirmationDialog;
+
+        removeDoctorConfirmationDialog = new Dialog(this);
+        removeDoctorConfirmationDialog.setContentView(R.layout.clinic_reject_appointment_dialog);
+        removeDoctorConfirmationDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        removeDoctorConfirmationDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_dialog_box));
+        removeDoctorConfirmationDialog.setCancelable(true);
+
+        MaterialButton confirmButton, cancelButton;
+        TextView detail = removeDoctorConfirmationDialog.findViewById(R.id.promptTextDetail);
+
+        confirmButton = removeDoctorConfirmationDialog.findViewById(R.id.confirmButton);
+        cancelButton = removeDoctorConfirmationDialog.findViewById(R.id.cancelButton);
+
+        String promptDetail = "Remove Dr " + doctor.getfName() + " "  + doctor.getlName();
+
+        detail.setText(promptDetail);
+
+        cancelButton.setOnClickListener(v -> {
+            removeDoctorConfirmationDialog.dismiss();
+        });
+
+        confirmButton.setOnClickListener(v -> {
+            deleteDoctor();
+        });
+
+        removeDoctorConfirmationDialog.show();
+    }
+
+    private void deleteDoctor(){
+        String uniqueDoctorId = doctor.getCode(); // Replace with the actual method to get the ID
+
+        // Delete the doctor document from Firestore
+        db.collection("users").document(uniqueDoctorId)
+                .delete()
+                .addOnSuccessListener(aVoid -> {
+                    // Document successfully deleted
+                    // Optionally, you can navigate to a different screen or perform other actions
+                    Toast.makeText(this, "Doctor Deleted", Toast.LENGTH_SHORT).show();
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    // Handle errors if the document deletion fails
+                    // You might want to log the error or show a Toast/Snackbar to the user
+                    Toast.makeText(this, "Doctor Delete Fail", Toast.LENGTH_SHORT).show();
+                });
     }
 }
